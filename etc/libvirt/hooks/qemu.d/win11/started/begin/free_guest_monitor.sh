27881ea02d91c9fbc91ee930ee77fa356970b25d
Mike Watkins <python@mikewatkins.ca>
2022-01-13 13:50:51 -0800

    fixed HOST_DISPLAY typo

--- etc/libvirt/hooks/qemu.d/win11/started/begin/free_guest_monitor.sh
+++ etc/libvirt/hooks/qemu.d/win11/started/begin/free_guest_monitor.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 source /etc/libvirt/hooks/kvm.conf
 
-# Passing through the DISPLAY variable in this way as it's not in the runuser
-# environment Once vm started, monitor will autosense feed coming from
-# dedicated NVIDIA
+# Passing through the DISPLAY variable in this way to be consistent, as it's
+# not in the runuser environment needed for some commands.
+DISPLAY=$HOST_DISPLAY xrandr --output $GUEST_MONITOR --off
 
-runuser $RUNAS -c "DISPLAY=$HOST_DISPLAY xrandr --output $GUEST_MONITOR --off"
+# Once vm started, monitor will autosense feed coming from dedicated NVIDIA gpu
 
-# Instead of a blind killall barriers, this avoids an error condition if
+# Instead of a blind "killall barriers", this avoids an error condition if
 # barriers not running; this is a useful pattern for other commands, as libvirt
 # will abort the start command (and rewind the prepare actions) on errors,
 # which is what we want especially given we allocate 32GB (or whatever N
@@ -15,6 +15,6 @@ runuser $RUNAS -c "DISPLAY=$HOST_DISPLAY xrandr --output $GUEST_MONITOR --off"
 # scripts being run.
 if test -n "`pidof barriers`"; then killall -9 barriers; fi
 
-# Start barrier keyboard and mouse sharing daemon; client installed on VM
-runuser $RUNAS -c "DISPLAY=$DISPLAY /usr/bin/barriers --no-tray --address $HOST_IP:24800 --name $HOST_NAME --disable-crypto --disable-client-cert-checking -c /home/$RUNAS/.config/barrier/barrier.conf"
+# As user, start barrier keyboard and mouse sharing daemon; client installed on VM
+runuser $RUNAS -c "DISPLAY=$HOST_DISPLAY /usr/bin/barriers --no-tray --address $HOST_IP:24800 --name $HOST_NAME --disable-crypto --disable-client-cert-checking -c /home/$RUNAS/.config/barrier/barrier.conf"
 
