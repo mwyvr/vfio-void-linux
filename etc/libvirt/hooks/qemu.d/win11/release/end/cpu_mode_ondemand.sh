23df79162f668d4cc32c9168839b9e41d2acd604
Mike Watkins <python@mikewatkins.ca>
2022-01-21 11:23:55 -0800

    revert to my system default governor, schedutil

--- etc/libvirt/hooks/qemu.d/win11/release/end/cpu_mode_ondemand.sh
+++ etc/libvirt/hooks/qemu.d/win11/release/end/cpu_mode_ondemand.sh
@@ -4,6 +4,6 @@ source /etc/libvirt/hooks/kvm.conf
 ## Enable CPU governor on-demand mode
 for file in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; 
 do 
-	echo "ondemand" > $file; 
+	echo "schedutil" > $file; 
 done
 
