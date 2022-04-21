f2ab6548f7fdf346bce884bfcb229852751271e2
Mike Watkins <github@mikewatkins.ca>
2022-04-21 14:56:18 -0700

    cleanup

--- etc/libvirt/hooks/qemu.d/windows/started/begin/free_guest_monitor.sh
+++ etc/libvirt/hooks/qemu.d/windows/started/begin/free_guest_monitor.sh
@@ -3,7 +3,7 @@
 
 # Passing through the DISPLAY variable in this way to be consistent, as it's
 # not in the runuser environment needed for some commands.
-runuser $RUNAS -c "DISPLAY=$HOST_DISPLAY xrandr --output $GUEST_MONITOR --off"
+# runuser $RUNAS -c "DISPLAY=$HOST_DISPLAY $XRANDRPROG --output $GUEST_MONITOR --off"
 
 # Once vm started, monitor will autosense feed coming from dedicated NVIDIA gpu
 
@@ -16,5 +16,5 @@ runuser $RUNAS -c "DISPLAY=$HOST_DISPLAY xrandr --output $GUEST_MONITOR --off"
 # if test -n "`pidof barriers`"; then kill -9 `pidof barriers`; fi
 
 # As user, start barrier keyboard and mouse sharing daemon; client installed on VM
-runuser $RUNAS -c "DISPLAY=$HOST_DISPLAY /usr/bin/barriers --no-tray --address $HOST_IP:24800 --name $HOST_NAME --disable-crypto -c /home/$RUNAS/.config/barrier/barrier.conf"
+# runuser $RUNAS -c "DISPLAY=$HOST_DISPLAY /usr/bin/barriers --no-tray --address $HOST_IP:24800 --name $HOST_NAME --disable-crypto -c /home/$RUNAS/.config/barrier/barrier.conf"
 
--- etc/libvirt/hooks/qemu.d/windows/stopped/end/take_back_monitor.sh
+++ etc/libvirt/hooks/qemu.d/windows/stopped/end/take_back_monitor.sh
@@ -2,7 +2,7 @@
 . /etc/libvirt/hooks/kvm.conf
 
 # Once stopped, turn amu gpu feed back on; dwm will detect this.
-runuser -l $RUNAS -c "DISPLAY=$HOST_DISPLAY xrandr --output $GUEST_MONITOR --auto --right-of $HOST_MONITOR"
+# runuser -l $RUNAS -c "DISPLAY=$HOST_DISPLAY xrandr --output $GUEST_MONITOR --auto --right-of $HOST_MONITOR"
 
 # don't need barrier keyboard and mouse sharing any more
-if test -n "`pidof barriers`"; then kill -9 `pidof barriers`; fi
+# if test -n "`pidof barriers`"; then kill -9 `pidof barriers`; fi
